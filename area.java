1eeda030640f6738f89bee469efb000e436ac3cb
Tarun Dubey <tarun2019dubey@gmail.com>
2025-01-08 10:59:12 +0530

    area

---
+++ area.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class area{
+   public static void main(String[] args){
+         Scanner sc= new Scanner(System.in);
+		 
+		 System.out.println("enter the radius");
+		 int x= sc.nextInt();
+		 
+		
+		 
+		 double area=3.14*x*x;
+		 
+		 System.out.println(area);
+   }
+}
