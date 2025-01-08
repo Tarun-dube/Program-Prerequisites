50f28ca26a480504bcefb4a2ae8f6e8caf0c559a
Tarun Dubey <tarun2019dubey@gmail.com>
2025-01-08 10:44:24 +0530

    sum program

---
+++ sum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class sum{
+   public static void main(String[] args){
+         Scanner sc= new Scanner(System.in);
+		 
+		 System.out.println("enter the first number");
+		 int x= sc.nextInt();
+		 
+		 System.out.println("enter the second number");
+		 int y= sc.nextInt();
+		 
+		 int sum=x+y;
+		 
+		 System.out.println(sum);
+   }
+}
